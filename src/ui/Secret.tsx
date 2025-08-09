ee6a6a0953498cc467e90ce80a411df489156c87
Tatianka-hk <t0681689150@gmail.com>
2025-08-09 19:48:02 +0200

    change secret badge

--- src/ui/Secret.tsx
+++ src/ui/Secret.tsx
@@ -1,7 +1,12 @@
-export default function SecretComponent (){ return(
-<div
-    className="absolute top-0 right-0 -rotate-12 text-accent text-6xl font-bold border-8 border-accent p-2 opacity-80"
->
-    СЕКРЕТНО
-</div>
-) }
+import { useTranslation } from "react-i18next";
+
+export default function SecretComponent() {
+    const { t } = useTranslation();
+    return (
+        <div
+            className="absolute top-0 right-0 -rotate-12 text-accent text-6xl font-bold border-8 border-accent p-2 opacity-80"
+        >
+            {t('secret')}
+        </div>
+    )
+}
